aaacf352ed39b020777bf0ae4bf2f8578adb340e
Сергей Буркун <GizmoG2@live.ru>
2022-07-11 18:55:44 +0300

    Проверена новая система ввода.

--- Assets/MyAssets/Scripts/PlayerController.cs
+++ Assets/MyAssets/Scripts/PlayerController.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
+
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        Debug.Log(horizontalThrow);
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        Debug.Log(verticalThrow);
+        /*float horizontalThrow = Input.GetAxis("Horizontal");
         Debug.Log(horizontalThrow);
 
         float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        Debug.Log(verticalThrow);*/
     }
 }
