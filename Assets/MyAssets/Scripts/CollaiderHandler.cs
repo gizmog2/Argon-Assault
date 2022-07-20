9d96c018a97810ffe40fe2599920288519fe3833
Сергей Буркун <GizmoG2@live.ru>
2022-07-20 21:21:43 +0300

    Добавлен реакция на столкновения.

--- Assets/MyAssets/Scripts/CollaiderHandler.cs
+++ Assets/MyAssets/Scripts/CollaiderHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollaiderHandler : MonoBehaviour
 {
@@ -11,6 +12,24 @@ public class CollaiderHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        StartCrashSecuence();
         Debug.Log("We triggered with " + other.gameObject.name);
+    }
+
+    void StartCrashSecuence()
+    {
+        DisableMovement();
+        Invoke("ReloadLevel", 1f);
+    }
+
+    void DisableMovement()
+    {
+        GetComponent<PlayerController>().enabled = false;
+    }
+
+    void ReloadLevel()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentLevel);
     }
 }
