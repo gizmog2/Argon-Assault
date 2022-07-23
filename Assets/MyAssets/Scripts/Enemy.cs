db4f25587b7272c8299b91b34a301c8ca30dfd89
Сергей Буркун <GizmoG2@live.ru>
2022-07-23 17:02:24 +0300

    Создан эфект взрыва для врага и настроен.

--- Assets/MyAssets/Scripts/Enemy.cs
+++ Assets/MyAssets/Scripts/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] Transform parrent;
+
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parrent;
         Destroy(gameObject);
     }
 }
