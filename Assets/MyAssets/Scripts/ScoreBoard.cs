6a3b5a2f2c61839451ab7e266f55668289ec9839
Сергей Буркун <GizmoG2@live.ru>
2022-07-24 18:58:30 +0300

    Прописано отображение рекорда в интерфейсе.

--- Assets/MyAssets/Scripts/ScoreBoard.cs
+++ Assets/MyAssets/Scripts/ScoreBoard.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score = 0;
+    //[SerializeField] TextMesh scoreText;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        
+    }
 
     public void IncreaceScore(int AmountToIncreace)
     {
         score += AmountToIncreace;
-        Debug.Log($"Curent score is {score}.");
+        scoreText.text = score.ToString();
+        //Debug.Log($"Curent score is {score}.");
     }
 }
